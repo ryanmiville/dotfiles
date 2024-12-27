0ff98fd4e67d64adda52cf7536b4d42f052a8964
Ryan Miville <ryan.miville@terminus.com>
2024-12-27 15:32:28 -0500

    wilder with tokyo night colors

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -41,27 +41,27 @@ return { -- Autocompletion
 		local cmp = require("cmp")
 
 		-- `/` cmdline setup.
-		cmp.setup.cmdline("/", {
-			mapping = cmp.mapping.preset.cmdline(),
-			sources = {
-				{ name = "buffer" },
-			},
-		})
+		-- cmp.setup.cmdline("/", {
+		-- 	mapping = cmp.mapping.preset.cmdline(),
+		-- 	sources = {
+		-- 		{ name = "buffer" },
+		-- 	},
+		-- })
 		--
 		-- -- `:` cmdline setup.
-		cmp.setup.cmdline(":", {
-			mapping = cmp.mapping.preset.cmdline(),
-			sources = cmp.config.sources({
-				{ name = "path" },
-			}, {
-				{
-					name = "cmdline",
-					option = {
-						ignore_cmds = { "Man", "!" },
-					},
-				},
-			}),
-		})
+		-- cmp.setup.cmdline(":", {
+		-- 	mapping = cmp.mapping.preset.cmdline(),
+		-- 	sources = cmp.config.sources({
+		-- 		{ name = "path" },
+		-- 	}, {
+		-- 		{
+		-- 			name = "cmdline",
+		-- 			option = {
+		-- 				ignore_cmds = { "Man", "!" },
+		-- 			},
+		-- 		},
+		-- 	}),
+		-- })
 
 		local luasnip = require("luasnip")
 		luasnip.config.setup({})
