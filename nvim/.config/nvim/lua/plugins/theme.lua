ed23dbc7b77fe92e753327ccdd10dd4370d49d43
Ryan Miville <ryanmiville@gmail.com>
2025-12-30 13:06:12 -0500

    catppuccin mocha

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,10 +1,22 @@
+-- return {
+-- 	"folke/tokyonight.nvim",
+-- 	lazy = false,
+-- 	priority = 1000,
+-- 	opts = {},
+-- 	init = function()
+-- 		-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+-- 		vim.cmd.colorscheme("tokyonight-storm")
+-- 	end,
+-- }
+
 return {
-	"folke/tokyonight.nvim",
-	lazy = false,
+	"catppuccin/nvim",
+	name = "catppuccin",
 	priority = 1000,
-	opts = {},
-	init = function()
-		-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-		vim.cmd.colorscheme("tokyonight-storm")
+	config = function()
+		require("catppuccin").setup({
+			auto_integrations = true,
+		})
+		vim.cmd.colorscheme("catppuccin-mocha")
 	end,
 }
