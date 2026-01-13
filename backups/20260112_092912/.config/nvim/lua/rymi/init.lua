cd6aea42969e9823d1aa89497caffcb9f3959076
Ryan Miville <ryanmiville@gmail.com>
2026-01-13 08:44:47 -0500

    updates

--- backups/20260112_092912/.config/nvim/lua/rymi/init.lua
+++
@@ -1,9 +0,0 @@
-require("rymi.options")
-require("rymi.lazy")
-require("rymi.keymaps")
-require("rymi.highlight_yank")
-require("rymi.vertical_help")
-require("rymi.toggle_diagnostics")
-require("rymi.copy_file_path_to_clipboard")
-require("rymi.set_file_type")
-
