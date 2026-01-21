5d4688273c5d0e9d877344c5b3f72f47895856e5
Ryan Miville <ryanmiville@gmail.com>
2026-01-21 11:40:48 -0500

    updates

--- home/.config/nvim/lua/plugins/lsp.lua
+++ home/.config/nvim/lua/plugins/lsp.lua
@@ -109,6 +109,16 @@ return {
 			vim.api.nvim_create_autocmd("LspAttach", {
 				group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
 				callback = function(event)
+					local bufnr = event.buf
+					local bufname = vim.api.nvim_buf_get_name(bufnr)
+
+					-- Detach from non-file buffers (codediff, etc.)
+					if bufname == "" or bufname:match("^diffview://") or bufname:match("^fugitive://") then
+						vim.schedule(function()
+							vim.lsp.buf_detach_client(bufnr, event.data.client_id)
+						end)
+						return
+					end
 					map_lsp_keybinds(event.buf)
 				end,
 			})
